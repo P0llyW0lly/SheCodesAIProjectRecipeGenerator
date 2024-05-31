478b35f18a41d835ac0a96920ea50a7ff3ed1175
P0llyW0lly <paula.h33nan@gmail.com>
2024-05-31 15:41:12 +0100

    Added AI Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: ""
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "Haggis, Neeps and Tatties",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  let ingredientInput = document.querySelector("#ingredient");
+  let apiKey = "478d0990d462befaftab553fodf9bd43";
+  let prompt = `generate a simple reciple including ${ingredientInput.value} in basic HTML and seperate lines with <br />. Do not say in basic HTML in the response`
+  let context = "you are a cuisine loving AI assistant who can suggest short, quick recipes";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
