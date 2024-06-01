bb67aeba416b9ec06711adda19c62c267ca55483
P0llyW0lly <paula.h33nan@gmail.com>
2024-06-01 10:01:53 +0100

    Updated UI prompt

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
 
   let ingredientInput = document.querySelector("#ingredient");
   let apiKey = "478d0990d462befaftab553fodf9bd43";
-  let prompt = `generate a simple reciple including ${ingredientInput.value} in HTML separate each line with a <br />. Do not say in basic HTML in the response`
+  let prompt = `generate a simple reciple including ${ingredientInput.value} in HTML format. When writing the recipe separate the ingredients with an HTML <br />, separate each instruction or step with a <br />. Do not say in basic HTML in the response`
   let context = "you are a cuisine loving AI assistant who can suggest short, quick recipes";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
